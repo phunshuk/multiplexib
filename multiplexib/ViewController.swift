875b07e155605b0a241819a127c14c500ea291f3
OSX <osx@orem-mac.local>
2015-09-07 10:14:52 +0530

    Initial Commit

---
+++ multiplexib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multiplexib
+//
+//  Created by OSX on 07/09/15.
+//  Copyright (c) 2015 OSX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
